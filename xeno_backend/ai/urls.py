d56bd8062156a4455805b2deff5716abe162af09
Sanjay S <133774180+sk-sanju@users.noreply.github.com>
2025-06-25 00:22:31 +0530

    Delete xeno_backend directory

--- xeno_backend/ai/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import GenerateSoundscape
-from django.http import HttpResponse
-
-urlpatterns = [
-    path('<str:mood>/', GenerateSoundscape.as_view(), name='generate-soundscape'),
-    #path('test/', lambda request: HttpResponse("It works!")),
-
-]
