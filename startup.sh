558ea55a542bed1ec1e9a16eb1f3766d43e43152
Hari Govind M <harigovindappu4926@gmail.com>
2025-08-03 18:03:42 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,7 +3,5 @@
 # Install packages if not already installed
 apt-get update && apt-get install -y ffmpeg fluidsynth
 
-pip install -r requirements.txt
-
 # Start your app
 gunicorn main:app --bind 0.0.0.0:8000
